8dedf5ab9fbb662ef6053e21c405df25a9633a79
Alpha 2 <8784iirv5@gmail.com>
2023-10-08 14:37:46 +0200

    PrimeNumbers

--- Program.cs
+++ Program.cs
@@ -6,10 +6,44 @@ using System.Threading.Tasks;
 
 namespace CSharp_PrimeNumbers
 {
-    internal class Program
+    class Program
     {
+        static bool IsPrimeNumber(int number)
+        {
+            if (number <= 1)
+                return false;
+
+            if (number <= 3)
+                return true;
+
+            if (number % 2 == 0 || number % 3 == 0)
+                return false;
+
+            for (int i = 5; i * i <= number; i += 6)
+                if (number % i == 0 || number % (i + 2) == 0)
+                    return false;
+
+            return true;
+        }
+
         static void Main(string[] args)
         {
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < n; i++)
+                    if (IsPrimeNumber(i))
+                        Console.Write(i + " ");
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine();
+
+            Console.ReadKey();
         }
     }
 }
